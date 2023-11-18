c6e6eeb74fc3d01c668d605e73a23cdcd19fcc7e
Huynh Phan Nhat Vy <71034074+nhatvy118@users.noreply.github.com>
2023-11-18 11:12:51 +0700

    css nav

--- script.js
+++ script.js
@@ -24,3 +24,59 @@ function updateCarousel() {
   const newTransformValue = -currentSlide * 100 + '%';
   carousel.style.transform = 'translateX(' + newTransformValue + ')';
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const searchInput = document.querySelector('.search-bar-input input');
+  const searchSelect = document.querySelector('.search-facet-value');
+  const searchResults = document.querySelector('.search-results');
+
+  const books = [
+      { "id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
+      { "id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee" },
+      { "id": 3, "title": "1984", "author": "George Orwell" }
+  ];
+
+  function searchBooks() {
+      const searchTerm = searchInput.value.toLowerCase();
+      const searchCategory = searchSelect.value.toLowerCase(); // Use the 'value' property for the select element
+      const filteredBooks = filterBooks(books, searchTerm, searchCategory);
+
+      // Clear previous search results
+      searchResults.innerHTML = '';
+
+      // Display search results
+      if (filteredBooks.length > 0) {
+          const resultList = document.createElement('ul');
+          filteredBooks.forEach(book => {
+              const listItem = document.createElement('li');
+              listItem.textContent = `${book.title} by ${book.author}`;
+              resultList.appendChild(listItem);
+          });
+          searchResults.appendChild(resultList);
+      } else {
+          const noResultsItem = document.createElement('li');
+          noResultsItem.textContent = 'No matching books found.';
+          searchResults.appendChild(noResultsItem);
+      }
+  }
+
+  function filterBooks(books, term, category) {
+      return books.filter(book => {
+          const bookValue = book[category].toLowerCase();
+          return bookValue.includes(term);
+      });
+  }
+
+  // Event listeners
+  searchInput.addEventListener('input', searchBooks);
+  searchSelect.addEventListener('change', searchBooks);
+
+  // Optional: Trigger search on form submit
+  const searchForm = document.querySelector('.search-bar-input');
+  searchForm.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevent the form from submitting (which would cause a page refresh)
+      searchBooks();
+  });
+});
